b825f133aa80a1e28fe10b4bda1fefc3d17bf195
Selcuk <1202320@student.roc-nijmegen.nl>
2023-01-20 13:16:14 +0100

    Create ss.js

---
+++ javascript/ss.js
@@ -0,0 +1,11 @@
+var out = document.getElementById("navigation");
+if(out.style.display === 'block'){
+
+  out.classList.add("animate__animated"); 
+  out.classList.add("animate__fadeOutTopRight");
+
+
+}
+else{
+  console.log('jammer dan')
+}
